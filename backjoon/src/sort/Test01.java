94a5c4f52117a2179a34ab5a00a3c373b8e0211e
silverz <liisa9601@naver.com>
2021-08-23 23:40:02 +0900

    sort

---
+++ backjoon/src/sort/Test01.java
@@ -0,0 +1,42 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test01 {
+public static void main(String[] args) {
+	
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int[][] arr=new int[n][2];
+	
+	
+	for(int i=0;i<n;i++) {
+		for (int j=0;j<2;j++) {
+			arr[i][j]=sc.nextInt();
+		}
+	}
+	
+	Arrays.sort(arr,(num1,num2)->{
+		
+		if(num1[1]==num2[1])
+			return Integer.compare(num1[0], num2[0]);
+		else
+			return Integer.compare(num1[1], num2[1]);
+		
+	
+	});
+	
+	
+	
+	
+	
+	for(int a=0;a<n;a++) {
+		for(int b=0;b<2;b++) {
+			System.out.print(arr[a][b]+" ");
+		}
+		System.out.println();
+	}
+	
+}
+}
---
+++ backjoon/src/sort/Test02.java
@@ -0,0 +1,39 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test02 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	String[] arr=new String[n+1];
+	arr[n]="";
+	String temp;
+	
+	for(int i=0;i<n;i++) {
+		arr[i]=sc.next();
+	}
+	
+	Arrays.sort(arr);
+
+	for(int i=0;i<n+1;i++) {
+		for(int j=0;j<n;j++) {
+			if(arr[j].length()>arr[j+1].length()) {
+				temp=arr[j+1];
+				arr[j+1]=arr[j];
+				arr[j]=temp;
+			}
+			
+		}
+		
+	}
+	
+	for(int i=0;i<n;i++) {
+		if(arr[i].length()==arr[i+1].length()){
+			if(arr[i].contains(arr[i+1])) continue;
+		}
+		System.out.println(arr[i+1]);
+	}
+}
+}
